03edee5c489a5f661eacf7d527ff6cf39220f0d7
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-09 22:34:18 +0100

    Integrate Desktop header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./**/*{.html,js}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     fontFamily: {
       "body": ["Be Vietnam Pro", "sans-serif"],
     },
