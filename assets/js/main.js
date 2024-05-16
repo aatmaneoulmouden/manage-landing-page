735936a348e8a6dc06f824610e50b73969c34b38
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-05-16 22:16:00 +0100

    Header mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
 /**
+ * Header menu 
+ */
+const openMenuBtn = document.querySelector('#open-mobile-menu');
+const closeMenuBtn = document.querySelector('#close-mobile-menu');
+const menu = document.querySelector('#menu');
+
+openMenuBtn.addEventListener('click', () => {
+    openMenuBtn.classList.add('hidden');
+    menu.classList.remove('hidden');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+    openMenuBtn.classList.remove('hidden');
+    menu.classList.add('hidden');
+})
+
+/**
  * Testimonials slider
  */
-
 var swiper = new Swiper(".testimonialSwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -15,11 +31,11 @@ var swiper = new Swiper(".testimonialSwiper", {
             spaceBetween: 20,
         },
         768: {
-            slidesPerView: 4,
+            slidesPerView: 3,
             spaceBetween: 40,
         },
         1024: {
-            slidesPerView: 3,
+            slidesPerView: 4,
             spaceBetween: 30,
         },
     },
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   content: ["./**/*{.html,js}"],
   theme: {
     container: {
+      center: true,
       padding: {
         DEFAULT: '1rem',
         sm: '2rem',
-        lg: '5rem',
+        lg: '3rem',
         xl: '6rem',
         '2xl': '7rem',
       },
@@ -20,7 +21,8 @@ module.exports = {
       "dark-grayish-blue": "hsl(227, 12%, 61%)",
       "very-dark-blue": "hsl(233, 12%, 13%)",
       "very-pale-red": "hsl(13, 100%, 96%)",
-      "very-light-gray": "hsl(0, 0%, 98%)"
+      "very-light-gray": "hsl(0, 0%, 98%)",
+      "white": "#FFFFFF"
     },
     extend: {},
   },
